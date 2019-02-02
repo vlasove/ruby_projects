05bba0f699f25e5bd1e3540362902c1f46a632cb
Evgen <evgeny_vlasov@yahoo.com>
2019-02-02 23:31:25 +0300

    User Interface

---
+++ second.rb
@@ -0,0 +1,12 @@
+puts("Enter your name: ")
+name = gets
+puts("Cool, your name is "+name)
+
+puts("Enter FIrst number: ")
+x = gets.chomp().to_i
+puts("Enter Second Numer: ")
+y = gets.chomp().to_i
+
+res = x + y
+
+puts(x+y)
